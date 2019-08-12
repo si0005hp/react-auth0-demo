36595652f2d42b4e6ea48a016242631675a12930
si0005hp <si0005hp@gmail.com>
2019-08-13 03:29:55 +0900

    Fin pt2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
+import ExternalApi from "./components/ExternalApi";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route path="/" exact />
           <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </BrowserRouter>
     </div>
